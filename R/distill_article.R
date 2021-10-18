cb1e455e42a5e90acff3c54521bde7cbe6fbb898
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-10-18 16:05:33 +1100

    Fix \@ref() referencing for PDF output

--- R/distill_article.R
+++ R/distill_article.R
@@ -327,13 +327,24 @@ rjournal_web_article <- function(toc = FALSE,
 rjournal_pdf_article <- function(..., self_contained = FALSE) {
   fmt <- rticles::rjournal_article(...)
   post_process <- fmt$post_processor
-  fmt$post_processor <- function(...) {
+  fmt$post_processor <- function(metadata, utf8_input, output_file, clean, verbose) {
+
+    # Replace \@ref(***) with \ref{***}
+    file <- xfun::with_ext(output_file, '.tex')
+    lines <- xfun::read_utf8(file)
+    ## From bookdown/R/latex.R
+    lines = gsub(
+      '(?<!\\\\textbackslash{})@ref\\(([-/:[:alnum:]]+)\\)', '\\\\ref{\\1}', lines,
+      perl = TRUE
+    )
+    xfun::write_utf8(lines, file)
+
     sty_origin <- list.files(system.file("tex", package = "rjdistill"),
                              full.names = TRUE)
     sty_dest <- file.path(".", basename(sty_origin))
     copied <- file.copy(sty_origin, sty_dest)
     on.exit(unlink(sty_dest[copied]))
-    post_process(...)
+    post_process(metadata, utf8_input, output_file, clean, verbose)
   }
   # output_format(
   #   knitr = knitr_options(),
